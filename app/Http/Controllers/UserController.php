04b81d379b2a874b4dc4495c0a417d808e3035df
WGuandique-2005 <wguandiquerivera07@gmail.com>
2025-05-30 22:30:25 -0600

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -329,7 +329,6 @@ class UserController extends Controller
             $data = $request->validate([
                 'name' => 'required|string|max:255',
                 'username' => 'required|string|max:255|unique:users,username,' . $user->id,
-                'email' => 'required|email|unique:users,email,' . $user->id,
                 'password' => 'nullable|string|min:8|confirmed',
             ]);
 
