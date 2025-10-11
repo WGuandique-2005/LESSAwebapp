378f14d312fdd90a1eb132dc6bdbfdef74c35c0d
WGuandique-2005 <wguandiquerivera07@gmail.com>
2025-10-10 20:15:10 -0600

    commit

--- app/Http/Controllers/RecompensasUsuarioController.php
+++ app/Http/Controllers/RecompensasUsuarioController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Recompensa;
+use App\Models\PuntosUsuario;
+use App\Models\Nivel;
 use App\Models\RecompensasUsuario;
 
 class RecompensasUsuarioController extends Controller
 {
-    //
+    // Quisiera crear un método para asignar recompensas a los usuarios basado en sus puntos, cuanto acumulen cierto
+    // puntaje por seccion, que salte ese evento y se le asigne la recompensa.
+
+    public function asignarRecompensas()
+    {
+        // Mañana lo hago :v
+    }
 }
