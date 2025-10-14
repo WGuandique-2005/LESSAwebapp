22fdb6a063241e4d75e078992a5001bfe63919f2
WGuandique-2005 <wguandiquerivera07@gmail.com>
2025-10-14 16:48:56 -0600

    commit

--- app/Http/Controllers/NivelesController.php
+++ app/Http/Controllers/NivelesController.php
@@ -18,96 +18,96 @@ class NivelesController extends Controller
         return $data;
     }
 
-    function abecedario(){
+    public function abecedario(){
         return view('practica_section.abecedario');
     }
 
-    function abecedario_adivina(){
+    public function abecedario_adivina(){
         $abecedarioData = $this->cargarDatos('abecedario');
         return view('practica_section.abecedario.A1_adivina', compact('abecedarioData'));
     }
 
 
-    function abecedario_memorama(){
+    public function abecedario_memorama(){
         $abecedarioData = $this->cargarDatos('abecedario');
         return view('practica_section.abecedario.a2_memorama', compact('abecedarioData'));
     }
 
-    function abecedario_conecta(){
+    public function abecedario_conecta(){
         $abecedarioData = $this->cargarDatos('abecedario');
         return view('practica_section.abecedario.a3_conecta', compact('abecedarioData'));
     }
 
-    function abecedario_extra(){
+    public function abecedario_extra(){
         return view('practica_section.abecedario.a4_extra');
     }
 
-    function numeros(){
+    public function numeros(){
         return view('practica_section.numeros');
     }
 
-    function numeros_adivina(){
+    public function numeros_adivina(){
         $numerosData = $this->cargarDatos('numeros');
         return view('practica_section.numeros.A1_adivina', compact('numerosData'));
     }
 
-    function numeros_memorama(){
+    public function numeros_memorama(){
         $numerosData = $this->cargarDatos('numeros');
         return view('practica_section.numeros.A2_memorama', compact('numerosData'));
     }
 
-    function numeros_conecta(){
+    public function numeros_conecta(){
         $numerosData = $this->cargarDatos('numeros');
         return view('practica_section.numeros.A3_conecta', compact('numerosData'));
     }
 
-    function numeros_extra(){
+    public function numeros_extra(){
         return view('practica_section.numeros.A4_extra');
     }
 
-    function saludos(){
+    public function saludos(){
         return view('practica_section.saludos');
     }
 
-    function saludos_adivina(){
+    public function saludos_adivina(){
         $saludosData = $this->cargarDatos('saludos');
         return view('practica_section.saludos.A1_adivina', compact('saludosData'));
     }
 
-    function saludos_memorama(){
+    public function saludos_memorama(){
         $saludosData = $this->cargarDatos('saludos');
         return view('practica_section.saludos.A2_memorama', compact('saludosData'));
     }
 
-    function saludos_conecta(){
+    public function saludos_conecta(){
         $saludosData = $this->cargarDatos('saludos');
         return view('practica_section.saludos.A3_conecta', compact('saludosData'));
     }
 
-    function saludos_extra(){
+    public function saludos_extra(){
         return view('practica_section.saludos.A4_extra');
     }
 
-    function salud(){
+    public function salud(){
         return view('practica_section.salud');
     }
 
-    function salud_adivina(){
+    public function salud_adivina(){
         $saludData = $this->cargarDatos('salud');
         return view('practica_section.salud.A1_adivina', compact('saludData'));
     }
 
-    function salud_memorama(){
+    public function salud_memorama(){
         $saludosData = $this->cargarDatos('saludos');
         return view('practica_section.salud.A2_memorama', compact('saludosData'));
     }
 
-    function salud_conecta(){
+    public function salud_conecta(){
         $saludData = $this->cargarDatos('salud');
         return view('practica_section.salud.A3_conecta', compact('saludData'));
     }
 
-    function salud_extra(){
+    public function salud_extra(){
         return view('practica_section.salud.A4_extra');
     }  
 }
